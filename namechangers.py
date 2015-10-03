c17917efce5b296d14bbd35026d27888d6891ac0
ph03n1x9000 <ph03n1x9000@hotmail.com>
2015-10-03 19:44:27 -0400

    Update namechangers.py

--- namechangers.py
+++ namechangers.py
@@ -46,39 +46,29 @@
 # 09-08-2015 - 1.0.1 - ph03n1x
 #   * rewrote entire code for better detection. Only works with b3 1.10 and later
 #   * Credit given to NPR|pyr0 because I used his idea
-# 09-09-2015 - 1.0.2 - ph03n1x
-#   * Added backward compatibility to v1.9.2
-
 
 __author__ = 'NRP|pyr0, ph03n1x'
 __version__ = '1.0.2'
 
 import b3
 import b3.events
-import b3.plugin
 from b3 import functions
-
+import b3.plugin
 
 class NamechangersPlugin(b3.plugin.Plugin):
-    _storedClients = {}  # Stores the clients that have changed names
+    _storedClients = {}
     _default_messages = {'kick': 'Player $name Kicked for too many namechanges (GUID: $guid)',
                          'tempban': 'Player $name Temp Banned for too many namechanges (GUID: $guid)',
                          'permban': 'Player $name PermBanned for too many namechanges (GUID: $guid)'
                          }
 
     def onStartup(self):
-        try:
-            # Register events using new dispatch system
-            self.registerEvent('EVT_CLIENT_NAME_CHANGE', self.nameChangeOccurred)
-            self.registerEvent('EVT_CLIENT_KICK', self.onPenalty)
-            self.registerEvent('EVT_CLIENT_BAN_TEMP', self.onPenalty)
-            self.registerEvent('EVT_CLIENT_BAN', self.onPenalty)
-        except:
-            # Keep backwards compatibility
-            self.registerEvent(b3.events.EVT_CLIENT_NAME_CHANGE)
-            self.registerEvent(b3.events.EVT_CLIENT_KICK)
-            self.registerEvent(b3.events.EVT_CLIENT_BAN_TEMP)
-            self.registerEvent(b3.events.EVT_CLIENT_BAN)
+        # Register events using new dispatch system
+        self.registerEvent('EVT_CLIENT_NAME_CHANGE', self.nameChangeOccurred)
+        self.registerEvent('EVT_CLIENT_KICK', self.onPenalty)
+        self.registerEvent('EVT_CLIENT_BAN_TEMP', self.onPenalty)
+        self.registerEvent('EVT_CLIENT_BAN', self.onPenalty)
+        self.registerEvent('EVT_CLIENT_DISCONNECT', self.onPenalty)
 
     def onLoadConfig(self):
         try:
@@ -127,16 +117,6 @@ class NamechangersPlugin(b3.plugin.Plugin):
             for (penalty, message) in self.config.items('messages'):
                 self._default_messages[penalty] = message
 
-    ########################### BACKWARD COMPATIBILITY SECTION #####################
-    def onEvent(self, event):
-        if event.type == b3.events.EVT_CLIENT_NAME_CHANGE:
-            self.nameChangeOccurred(event)
-        elif event.type == b3.events.EVT_CLIENT_KICK or \
-            event.type == b3.events.EVT_CLIENT_BAN_TEMP or \
-            event.type == b3.events.EVT_CLIENT_BAN:
-            self.onPenalty(event)
-
-
     ########################### EVENT HANDLING #####################################
     def nameChangeOccurred(self, event):
         """\
@@ -144,6 +124,7 @@ class NamechangersPlugin(b3.plugin.Plugin):
         """
         #Client has changed name. Store his info in case he changes again
         client = event.client
+        self.verbose('Checking %s for namechanges' % client.name)
 
         # Check if client level is to be ignored
         if client.maxLevel >= self.ignoreLevel:
@@ -151,61 +132,82 @@ class NamechangersPlugin(b3.plugin.Plugin):
             return
 
         # Check if client has changed name before. If not, initialize client name storage.
-        if client.guid not in self._storedClients:
-            self._storedClients[client.guid] = list()
+        if client not in self._storedClients:
+            self._storedClients[client] = list()
+        elif client in self._storedClients:
+            # Double check that we have the right client
+            a = self.checkIfSame(client)
+            if not a:
+                self.debug('It seems that there was a mix up with the clients. Cancelling')
+                return
 
         # Save client info
-        self._storedClients[client.guid].append(client.name)
+        self._storedClients[client].append(client.name)
 
         # Write to log if needed
-        n = 'Client with GUID: %s has changed name. Used names: %s' % (client.guid, self._storedClients[client.guid])
+        n = 'Client with GUID: %s has changed name. Used names: %s' % (client.guid, (', ').join(self._storedClients[client]))
         self.callLog(n)
 
         # Check if client change names has reached tolerated limit
-        if len(self._storedClients[client.guid]) >= self.maxnames:
+        if len(self._storedClients[client]) >= self.maxnames:
             self.penalize(client)
-        elif len(self._storedClients[client.guid]) < self.maxnames:
+        elif len(self._storedClients[client]) < self.maxnames:
             self.notifyAdmins(client)
 
     def onPenalty(self, event):
         """\
-        Handle EVT_CLIENT_BAN_TEMP, EVT_CLIENT_BAN and EVT_CLIENT_KICK
+        Handle EVT_CLIENT_BAN_TEMP, EVT_CLIENT_DISCONNECT, EVT_CLIENT_BAN and EVT_CLIENT_KICK
         We do it this way so that we know if client is manually penalized by admin
         """
         c = event.client
         # Check if client in this event is being watched. If so remove from stored clients
-        if c.guid in self._storedClients:
-            self._storedClients.pop(c.guid)
+        if c in self._storedClients:
+            self._storedClients.pop(c)
+            self.debug('Client: %s removed from stored list after penalized.' % c)
 
     ####################################### FUNCTIONING ##################################
     def penalize(self, client):
         self.debug('Too many name changes performed by GUID: %s. Penalizing' % client.guid)
         if self.action == 'kick':
-            client.kick(reason='Too many name changes', keyword='NameChanger', data='%s Namechanges' % len(self._storedClients[client.guid]))
+            client.kick(reason='Too many name changes', keyword='NameChanger', data='%s Namechanges' % len(self._storedClients[client]))
         elif self.action == 'tempban':
-            client.tempban(reason='Too many name changes', keyword='NameChanger', duration=self.duration, data='%s NameChanges' % len(self._storedClients[client.guid]))
+            client.tempban(reason='Too many name changes', keyword='NameChanger', duration=self.duration, data='%s NameChanges' % len(self._storedClients[client]))
         elif self.action == 'permban':
-            client.ban(reason='Too many name changes', keyword='NameChanger', data='%s Namechanges' % len(self._storedClients[client.guid]))
+            client.ban(reason='Too many name changes', keyword='NameChanger', data='%s Namechanges' % len(self._storedClients[client]))
         self.callLog('%s penalized for too many name changes. Penalty: %s' % (client.guid, self.action))
         param = {'name': client.name,
                  'guid': client.guid}
         self.console.say(self.getMessage(self.action, param))
 
     def callLog(self, data):
-            if self.logLocation:
-                try:
-                    filelog = ('%s' % self.logLocation)
-                    f = open(filelog, "a")
-                    f.write(data + '\n')
-                    f.close()
-                except:
-                    self.info(data)
+        if self.logLocation:
+            try:
+                filelog = ('%s' % self.logLocation)
+                f = open(filelog, "a")
+                f.write(data + '\n')
+                f.close()
+            except:
+                self.info(data)
 
     def notifyAdmins(self, player):
         if self.notify:
             for c in self.console.clients.getList():
                 if c.maxLevel >= self.notify:
-                    c.message('^1ALERT!^7 Player ^3%s ^7has changed their name ^3%s ^7times' % (player.id, len(self._storedClients[player.guid])))
+                    c.message('^1ALERT!^7 Player ^3%s ^7changed name ^3%s ^7times' % (player.cid, len(self._storedClients[player])))
+                    c.message('Last alias: %s' % self._storedClients[player][-2])
 
+    def checkIfSame(self, client):
+        """\
+        Making sure we don't mix up clients
+        :client: Client we are checking against stored list
+        """
+        checking = ''
+        for x in self._storedClients.keys():
+            if x == client:
+                checking = x
 
+        if client.ip == checking.ip and client.guid == checking.guid and client.cid == checking.cid:
+            return True
+        else:
+            return False
 
